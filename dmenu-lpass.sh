779147303d854f64975eb0bcbb7626dcd4142fe9
Joel Bradshaw <cincodenada@gmail.com>
2017-09-21 11:27:17 -0700

    Update to work with my current version of lpass
    There were rogue spaces that were breaking things

--- dmenu-lpass.sh
+++ dmenu-lpass.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 
-selection=`echo -e $(lpass ls | awk -F '[/[]' '{print $(NF-1)"\\\n"}') | dmenu`
+selection=`lpass ls | awk -F '[/[]' '$0 !~ /\/ \[id/ {gsub(/ $/,"",$(NF-1)); print $(NF-1)}' | dmenu -i`
 
+echo "<$selection>"
 if [ "$selection" != "" ]; then
-    lpass show -c --password $selection &
+    lpass show -c --password "$selection" &
 fi
